bd68f2e2d0a0917649b331548dbfd99cfe8d6b76
Sheldoer <1829523261@qq.com>
2021-03-02 21:59:55 +0800

    210302做的15年复试真题

--- interviewquestion/15/15.py
+++ interviewquestion/15/15.py
@@ -1,15 +1,29 @@
+import struct
 def _2015():
     """
     ##### `2015`年复试上机
     **要求：**
-    * 从网页上下载`input.dat`文件，里面是用二进制编写的，里面放了一堆`int`型的数，每个数占`4`个字节，每次读取两个，这两个数构成一个坐标.
+    * 从网页上下载`input.dat`文件，里面是用二进制编写的，里面放了一堆`int`型的数，
+    每个数占`4`个字节，每次读取两个，这两个数构成一个坐标.
     * 规定处于第一象限的数是有效点(即`x>0, y>0`的坐标)，问这么多点中有效点有多少个？
-    * 从键盘上输入`k`和`n`，从第一问中的有效点中找出距离小于`n`，距离小于`n`的点的个数要大于`k`，将它们以文本格式输出到文件中.
+    * 从键盘上输入`k`和`n`，从第一问中的有效点中找出距离小于`n`，距离小于`n`的点的个数要大于`k`，
+    将它们以文本格式输出到文件中.
     **程序：**
     """
+    with open("input.txt","rb")as fp:
+        size=struct.calcsize('i')
+        f=fp.read(size)
+        p=[]
+        while f:
+            value=struct.unpack('i',f)[0]
+            p.append(value)
+            f=fp.read(size)
+    fp.close()
+    '''
     with open('input.txt', encoding='utf-8') as f:
         p = f.read().split()
         p = list(map(int, p))
+    '''
     size = len(p) // 2
     ans = []
     for i in range(size):
@@ -58,3 +72,4 @@ def _2015():
         for point, d in ls:
             print('({}, {}), d={:.2f}'.format(point[0], point[1], d))
         print()
+_2015()
